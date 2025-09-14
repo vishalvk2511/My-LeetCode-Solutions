1630d9c02b9a3e8cc5804fdba9818095e9205050
Vishal Vinod Kumar <60807325+vishalvk2511@users.noreply.github.com>
2025-09-14 12:12:55 +0530

    Time: 1764 ms (5.00%) | Memory: 16.7 MB (44.04%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        res = {}
+
+        for s in strs:
+            key = tuple(sorted(s))
+            if key in res.keys():
+                res[key].append(s)
+            else:
+                res[key]=[s]
+        return list(res.values())
+        
